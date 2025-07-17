f9f68dc2217255aac77bbf23ef2fbbcb4611ab26
J-yejin <yejimy0524@gmail.com>
2025-07-18 00:25:12 +0900

    algorithm middle

---
+++ number_circulation.py
@@ -0,0 +1,22 @@
+T = int(input())
+
+for test_case in range(1,T+1):
+    print('#{number}'.format(number = test_case))
+    num = int(input())
+    mat =[]
+    for _ in range(num):
+        row = list(map(int, input().split(' ')))
+        mat.append(row)
+
+    for n in range(num - 1, -1, -1):
+        for i in range(num-1, -1, -1):
+            print(mat[i][num - n-1], end='')
+        print(' ', end='')
+        for i in range(num-1, -1, -1):
+            print(mat[n][i], end='')
+        print(' ', end='')
+        for i in range(0, num):
+            print(mat[i][n], end='')
+        print('')
+
+
